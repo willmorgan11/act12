32d5db13c0a7d9bf13807245867a2de7f8e06ba4
William Morgan <wmorgan2023@gmail.com>
2026-04-06 17:46:08 -0400

    Fixed crash issue in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/inventory_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const InventoryApp());
 }
 
