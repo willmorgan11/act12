c4f0d7ee3123133583189c7aacc5df48d671e368
William Morgan <wmorgan2023@gmail.com>
2026-04-06 16:02:25 -0400

    Reconfigured Firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.act12"
+        applicationId = "com.inclass.act12"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
